68aa1a14ed5904f8eb4110377819c69598fcaa3d
ShiroiKiku <ividevil@mail.ru>
2022-05-12 17:58:40 +0400

    main

---
+++ client/pages/auth.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const auth: NextPage = () => {
+    return (
+        <div className='container'>
+            <h1>Auth</h1>
+        </div>
+    );
+};
+
+export default auth;
---
+++ routes/auth.routes.js
@@ -0,0 +1,88 @@
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const config = require("config");
+const { Router } = require("express");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const router = Router();
+
+// /api/auth/register
+router.post(
+    "/register",
+    [
+        check("email", "Некорректный email").isEmail(),
+        check("password", "Минимальная длинна пароля 6").isLength({ min: 6 }),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    massage: "Некко данные регистрации",
+                });
+            }
+            const { email, password } = req.body;
+
+            const candidate = await User.findOne({ email });
+
+            if (candidate) {
+                res.status(400).json({ massage: "Почта занята" });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const user = new User({ email, password: hashedPassword });
+
+            await user.save();
+            res.status(201).json({ massage: "Пользователь создан" });
+        } catch (e) {
+            res.status(500).json({ massage: "Ошибка регистрации" });
+        }
+    }
+);
+
+// /api/auth/login
+router.post(
+    "/login",
+    [
+        check("email", "Введите нормлаьный email").normalizeEmail().isEmail(),
+        check("password", "неправильный пароль").exists(),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    massage: "Некко данные входа",
+                });
+            }
+
+            const { email, password } = req.body;
+
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({ massage: "No user" });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({ massage: "Not pass" });
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                config.get("jwtSecret"),
+                { expiresIn: "1h" }
+            );
+
+            res.json({ token, userId: user.id });
+        } catch (e) {
+            res.status(500).json({ massage: "Ошибка входа" });
+        }
+    }
+);
+
+module.exports = router;
